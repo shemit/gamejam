86e55b4be9a5d209f3c9c43d5289c908edb61568
Vu Ha <stregawolf@gmail.com>
2016-01-31 14:05:42 -0800

    Added walk towards play after win

--- ritual/Assets/Ritual/Scripts/GazeUI/QuitGazeUI.cs
+++ ritual/Assets/Ritual/Scripts/GazeUI/QuitGazeUI.cs
@@ -6,11 +6,14 @@ public class QuitGazeUI : BaseGazeUIComponent {
     public override void Activate()
     {
         base.Activate();
+#if UNITY_EDITOR
         Debug.Log("quiting");
         if (Application.isEditor)
         {
             UnityEditor.EditorApplication.isPlaying = false;
         }
+#endif
+
         Application.Quit();
     }
 }
