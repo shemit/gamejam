05d42c158a0a27a6893107c8b115485d41a860ef
Vu Ha <stregawolf@gmail.com>
2016-01-30 16:11:27 -0800

    FirstPassOnIntroCutScene
    They come in, look at each other, and then leave

---
+++ ritual/Assets/Ritual/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+    public float m_Speed = 5.0f;
+    public float m_MaxTurnDegreesPerSecond = 10.0f;
+
+    public bool MoveTo(Vector3 pos)
+    {
+        Vector3 dir = pos - transform.position;
+        dir.y = 0.0f;
+        float dist = dir.magnitude;
+        if (dist < 1.0f)
+        {
+            return true; // return true if we're within threshold
+        }
+        dir.Normalize();
+        LookDir(dir);
+        transform.position += transform.forward * m_Speed * Time.deltaTime;
+        return false;
+    }
+
+    public bool LookDir(Vector3 dir)
+    {
+        Quaternion desiredLookRotation = Quaternion.LookRotation(dir, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredLookRotation, m_MaxTurnDegreesPerSecond * Time.deltaTime);
+
+        return Vector3.Dot(dir, transform.forward) > 0.9f; // if dot product is greater than .9 then we're facing close enough to the desired dir
+    }
+}
